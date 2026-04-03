417bbc5977c7ce0cfe8a825f5056e3a24f65e349
Hemant Uppal <165366867+HemantUppal@users.noreply.github.com>
2026-04-03 20:35:26 +0530

    Update config.py

--- HospitalManagementSystem_23f2001648/config.py
+++ HospitalManagementSystem_23f2001648/config.py
@@ -3,4 +3,4 @@ import os
 class Config:
     SECRET_KEY = os.urandom(24)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///hospital_services.db'
