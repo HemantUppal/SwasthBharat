6bf4123d2bd6d3739810c6d178503bbb1c9431dd
Hemant Uppal <165366867+HemantUppal@users.noreply.github.com>
2026-04-03 22:47:22 +0530

    Update config.py

--- HospitalManagementSystem_23f2001648/config.py
+++ HospitalManagementSystem_23f2001648/config.py
@@ -1,16 +1,10 @@
 
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class Config:
     SECRET_KEY = os.urandom(24)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    db_url = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 
-    if db_url and db_url.startswith("postgres://"):
-        db_url = db_url.replace("postgres://", "postgresql://", 1)
 
-    SQLALCHEMY_DATABASE_URI = db_url
